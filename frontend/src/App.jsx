6ce41f83662761e022d7b126576ec545040ed843
Wambogo Hassan Sadat <wambogohassan63@gmail.com>
2025-09-04 16:27:46 +0300

    added landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import VerifyEmail from "./pages/VerifyEmail";
 import AdminDashboard from "./pages/AdminDashboard";
 import StudentDashboard from "./pages/studentDashboard";
 import { useState, useEffect } from "react";
+import LandingPage from "./Pages/LandingPage";
 import VotingPage from "./pages/VotingPage";
 
 // ProtectedRoute component to guard dashboard routes
